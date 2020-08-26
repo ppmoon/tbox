2e77ca03b22e4c999e160fd3064f2919ca9d1c10
ppmoon <272305610@qq.com>
2020-08-26 16:16:11 +0800

    add a time kit get from now to tomorrow during

--- time.go
+++ time.go
@@ -1,1 +1,11 @@
 package tbox
+
+import "time"
+
+// get from now to tomorrow midnight during
+func FromNowToTomorrowMidnight() time.Duration {
+	now := time.Now()
+	today := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
+	tomorrow := today.AddDate(0, 0, 1)
+	return tomorrow.Sub(now)
+}
