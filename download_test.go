bdfe7ce3a0311db6e8590bdc137a300c3f39fa38
ppmoon <272305610@qq.com>
2020-08-26 15:37:07 +0800

    add download

---
+++ download_test.go
@@ -0,0 +1,14 @@
+package tbox_test
+
+import "testing"
+import "tbox"
+
+func TestDownloadFileAndStore(t *testing.T) {
+	filePath := "logo.png"
+	url := "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+	err := tbox.DownloadFileAndStore(filePath, url)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+}
